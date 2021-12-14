b7b64d4be638fa8b1acd7a388023083839d708f2
tkmo29102 <trinem6668@gmail.com>
2021-12-14 11:23:56 +0100

    Revert "Merge branch 'main' of https://github.com/tkmo29102/2-semestereksamen"
    This reverts commit f4dedbd5da2f6f6852ae134bcecfd3f1f884b0d3, reversing
    changes made to f9f3857b81c5e14597f4c13a03ec68045e38e419.

--- javascript/julie.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("grid__btn2").addEventListener("click", function() {
-    document.querySelector(".tilmeldpopup").style.display = "flex";
-  })
-
-  document.getElementById("tilmeldpopup-kryds").addEventListener("click", function() {
-    document.querySelector(".tilmeldpopup").style.display = "none";
-  })
