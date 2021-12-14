e749618d4b290c2a81edcdc596946c60bf31e3a1
Mariam <maribisult@hotmail.com>
2021-12-14 12:48:16 +0100

    javascript fil

---
+++ javascript/mariam.js
@@ -0,0 +1,10 @@
+function myFunction() {
+
+  var x = document.getElementById("box1");
+
+  if (x.style.fontSize === "27px") {
+    x.style.fontSize = "20px";
+  } else {
+    x.style.fontSize = "27px";
+  }
+}
