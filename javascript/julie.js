7312fc21ed06f130146c21ed7ecd91a99f70f84a
Julie Jalsøe <juliejalsoe4@gmail.com>
2021-12-14 11:29:42 +0100

    nj

---
+++ javascript/julie.js
@@ -0,0 +1,7 @@
+document.getElementById("grid__btn2").addEventListener("click", function() {
+    document.querySelector(".tilmeldpopup").style.display = "flex";
+  })
+
+  document.getElementById("tilmeldpopup-kryds").addEventListener("click", function() {
+    document.querySelector(".tilmeldpopup").style.display = "none";
+  })
