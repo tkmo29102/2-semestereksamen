2eb43ea08cbac9823607a59e2a362bc7ca4903fa
Nannatubaek <83956697+Nannatubaek@users.noreply.github.com>
2021-12-15 10:45:55 +0100

    lavet kontaktsiden om

--- javascript/nanna.js
+++ javascript/nanna.js
@@ -1,11 +1,27 @@
 //video Forside
 document.getElementById('vid').play();
 
-//kontaktside
-{
-function()
+//pensum
+let test = ["hest","hest","hest"];
+//loop
+for ( let i = 0; i < test.length; i++) {
+  console.log(test[i]);
+}
 
-var name = document.forms["myForm"]["name"];
-var email = document.forms["myForm"]["email"];
-var message = document.forms["myForm"]["message"];
+//kontaktside
+function validateForm()
+{
+    var name = document.forms["myForm"]["name"];
+    var email = document.forms["myForm"]["email"];
+    var message = document.forms["myForm"]["message"];
+
+    if (name.value == "")
+    {
+        document.getElementById('errorname').innerHTML="Skriv en gyldig email";
+        name.focus();
+        return false;
+    }else{
+        document.getElementById('errorname').innerHTML="";
+    }
+    return true;
 }
